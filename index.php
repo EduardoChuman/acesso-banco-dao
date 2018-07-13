2095d6623aac727915c1ca49acfc209ed04107d3
Eduardo Chiaki Chuman <c111710@corp.caixa.gov.br>
2018-07-13 16:58:49 -0300

    organizacao das classes em pasta e alteracao do config.php

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
 <?php 
+// VERIFICA SE EXISTEM ERROS DE EXECUÇÃO NO CÓDIGO
+ini_set('display_errors',1);
 
+// CHAMA O ARQUIVO DE VERIFICAÇÃO DE EXISTÊNCIA DAS CLASSES
 require_once("config.php");
 
+// CRIA UM OBJETO SQL
 $sql = new Sql();
 
-var_dump($sql);
+// SOLICITA UM SELECT AO BANCO
+$usuarios = $sql->select("SELECT 
+							NOME_CLIENTE
+							,[CPF/CNPJ]
+							,[EMAIL_PRINCIPAL]
+      						,[EMAIL_SECUNDARIO]
+  							,[EMAIL_RESERVA] 
+						FROM 
+							tbl_SIEXC_OPES_EMAIL_CLIENTES_CADASTRO");
 
-$usuarios = $sql->select("SELECT NOME_CLIENTE from tbl_SIEXC_OPES_EMAIL_CLIENTES_CADASTRO");
-
-echo json_encode($usuarios);
-
+echo json_encode($usuarios, JSON_UNESCAPED_SLASHES);
 
 ?>
